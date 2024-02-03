a24778ba36708f5a1bdf38946346ebf80fa72458
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-02-03 19:49:31 +0100

    feat: finish design for navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,10 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()]
+  // pages: {
+  //   // Ustaw domyślny layout dla wszystkich stron
+  //   "src/**/*.astro": {
+  //     layout: "./src/layouts/Base.astro"
+  //   }
+  // }
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = 'Bartek Sobina';
 export const SITE_DESCRIPTION = 'Bartek Sobina home page / digital garden';
+export const GITHUB_URL = 'https://github.com/Ressobe'
